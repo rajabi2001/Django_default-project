9ddd244b7fd5c933b0437fce8a95e538a4c908f2
rajabi2001 <mj.rajabi2001@gmail.com>
2021-06-02 21:37:03 +0430

    create admin

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,7 +4,15 @@ class Question(models.Model):
     question_text = models.CharField(max_length=150)
     pub_date = models.DateTimeField('date of publish')
 
+    def __str__(self):
+        return self.question_text
+
 class Choice(models.Model):
     question = models.ForeignKey(Question , on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=150)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+    
