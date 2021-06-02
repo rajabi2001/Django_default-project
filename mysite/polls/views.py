a76d72e092c1f29af8e59a90ecb995f2aa34d944
rajabi2001 <mj.rajabi2001@gmail.com>
2021-06-02 22:07:50 +0430

    update url AND add dertail view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,3 +7,10 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
